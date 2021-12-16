53655c879a996ff940efaf92ec7e712c8c40bb04
MatWsQ <miatiby@gmail.com>
2021-12-16 22:18:49 +0100

    Cwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,70 @@
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj imię:");
-        String imie = scan.next();
-        System.out.print("Podaj nazwisko:");
-        String nazwisko = scan.next();
-        System.out.print("Podaj wiek:");
-        int wiek = scan.nextInt();
-        System.out.print("Podaj numer Indeksu:");
-        int nr_indeksu = scan.nextInt();
-
-        System.out.printf("Imie:  %s Naziwsko: %s ", imie, nazwisko );
-        System.out.println("Wiek: "+wiek + " "+ "Numer Indeksu: "+nr_indeksu);
 
 
-        System.out.println(" ");
-        System.out.println("Podaj 1 liczbę: ");
-        int liczbaA = scan.nextInt();
-        System.out.println("Podaj 2 liczbę: ");
-        int liczbaB = scan.nextInt();
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        boolean d = false;
+        System.out.println("Operatory arytmetyczne:" + "\na = true\tb = false\tc = true\td= false");
 
-        System.out.println("Dodawanie = "+ (liczbaA + liczbaB));
-        System.out.println("Odejmowanie = "+ (liczbaA - liczbaB));
-        System.out.println("Mnożenie = "+ (liczbaA * liczbaB));
-        System.out.println("Dzielenie = "+ (liczbaA / liczbaB));
-        System.out.println("Modulo = "+ (liczbaA % liczbaB));
+        System.out.println("(a && b) : " + (a && b));
+        System.out.println("(a && c) : " + (a && c));
+        System.out.println("(a && d) : " + (a && d));
+        System.out.println("(b && a) : " + (b && a));
+        System.out.println("(b && c) : " + (b && c));
+        System.out.println("(b && d) : " + (b && d));
+        System.out.println("(c && a) : " + (c && a));
+        System.out.println("(c && b) : " + (c && b));
+        System.out.println("(c && d) : " + (c && d));
+        System.out.println("(d && a) : " + (d && a));
+        System.out.println("(d && b) : " + (d && b));
+        System.out.println("(d && c) : " + (d && c));
+
+        System.out.println("");
+
+        System.out.println("(a || b) : " + (a || b));
+        System.out.println("(a || c) : " + (a || c));
+        System.out.println("(a || d) : " + (a || d));
+        System.out.println("(b || a) : " + (b || a));
+        System.out.println("(b || c) : " + (b || c));
+        System.out.println("(b || d) : " + (b || d));
+        System.out.println("(c || a) : " + (c || a));
+        System.out.println("(c || b) : " + (c || b));
+        System.out.println("(c || d) : " + (c || d));
+        System.out.println("(d || a) : " + (d || a));
+        System.out.println("(d || b) : " + (d || b));
+        System.out.println("(d || c) : " + (d || c));
+
+        System.out.println("");
+
+        System.out.println("!a : " + !a);
+        System.out.println("!b : " + !b);
+        System.out.println("!c : " + !c);
+        System.out.println("!d : " + !d);
+
+        System.out.println("");
+
+        System.out.println("!(a && b) : " + !(a && b));
+        System.out.println("!(b && c) : " + !(b && c));
+        System.out.println("!(c && d) : " + !(c && d));
+
+        System.out.println("");
+
+        System.out.println("!((a && c) || (!a || b) && (!a || d) : " + !((a && c) || (!a || b) && (!a || d)));
+        System.out.println("!((a && c) && (!a || b) && (!a || d) || (!d || b) : " + !((a && c) && (!a || b) && (!a || d) || (!d || b)));
+
+        System.out.println();
+
+        int x = 2;
+        int y = 5;
+        int z = 2;
+
+        System.out.printf("Operatory porównania:" + "\nx = %d\ty = %d\tz = %d\n", x, y, z);
+
+        System.out.println("((x > y) || (a && b) || (b && d) && (x <= z)) : " + ((x > y) || (a && b) || (b && d) && (x <= z)));
     }
 }
