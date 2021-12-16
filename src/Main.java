3aca764e807ae8b83710b4c701ad54933fd1d693
MatWsQ <miatiby@gmail.com>
2021-12-16 22:13:05 +0100

    Cwiczenie 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* ĆW#1 */
+        System.out.println("Przykładowy Tekst");
+
+        System.out.printf("To %s tekst  %s .", "jest", "sformatowany");
+
+        System.out.print("Ala ma");
+        System.out.print(" Kota");
+    }
+}
+
