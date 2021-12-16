b51b00390896df150985e67a3c93e765e7f7847c
MatWsQ <miatiby@gmail.com>
2021-12-16 22:27:32 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Przykładowy Tekst");
+
+        System.out.printf("To %s tekst %s .%d", "jest", "sformatowany", 100);
+        System.out.println(" ");
+        System.out.print("Ala ma");
+        System.out.print(" Kota");
     }
 }
 
